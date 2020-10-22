bcc48aa5241c46c0c9c3ddc7409610fe7e81c836
yonekura <ynkr03@gmail.com>
2020-10-22 06:51:27 +0000

    updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   for ($i = 0; $i <= 1000000; $i++) {
     $x += sqrt($x);
   }
-  $a = substr(bin2hex(random_bytes($length)), 0, 5);
+  $a = substr(bin2hex(random_bytes(8)), 0, 5);
   $$a = [];
   for($j=0;$j<10000;$j++) {
       $$a[] = $j;
