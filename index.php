4ffc295a110a50edfe7ee2e257726dbff6238fd5
yonekura <ynkr03@gmail.com>
2020-10-22 06:33:04 +0000

    updated

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
   for ($i = 0; $i <= 1000000; $i++) {
     $x += sqrt($x);
   }
-  echo "first:".memory_get_usage() / (1024 * 1024)."MB\n";
-  $arr = [];
+  $a = substr(bin2hex(random_bytes($length)), 0, 5);
+  $$a = []
   for($j=0;$j<10000;$j++) {
-      $arr[] = $j;
+      $$a[] = $j;
   }
-  echo "first:".memory_get_usage() / (1024 * 1024)."MB\n";
+  echo "memory usage:".memory_get_usage() / (1024 * 1024)."MB\n";
   echo "OK!";
 ?>
