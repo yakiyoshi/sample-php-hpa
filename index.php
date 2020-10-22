14cacc69819bbea01fce646307998b1cbe00b8c5
yonekura <ynkr03@gmail.com>
2020-10-22 07:35:12 +0000

    updated

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-  /*
+  // CPU Stress Test
   $x = 0.0001;
   for ($i = 0; $i <= 1000000; $i++) {
     $x += sqrt($x);
   }
   */
+
+  // Memory Stress Test
   class Foo
   {
     public $var = '3.14159265359';
   }
-
   $baseMemory = memory_get_usage();
-
   for ( $i = 0; $i <= 100000; $i++ )
   {
     $a = new Foo;
     $a->self = $a;
   }
-  echo memory_get_usage() - $baseMemory, "\n";
+
   echo "OK!";
 ?>
